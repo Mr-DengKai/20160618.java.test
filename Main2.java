b602c0315d742faf67887eb7393442397f62ec13
Mr-DengKai <964809903@qq>
2016-06-18 19:12:40 +0800

    add Main2.java

---
+++ Main2.java
@@ -0,0 +1,15 @@
+package com.orilore.dk.test1;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 100;
+int b = 100;
+System.out.println(a==b);
+Integer a1= 129;
+Integer b1 = 129;
+System.out.println(a1.equals(b1));
+	}
+
+}
