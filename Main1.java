e3228b643c1277f6ab1f89423709401264be7477
Mr-DengKai <964809903@qq>
2016-06-18 19:12:28 +0800

    add Main1.java

---
+++ Main1.java
@@ -0,0 +1,16 @@
+package com.orilore.dk.test1;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 100;
+int b  =a+a;
+Integer b1 = b+b; //将b自动拆箱结果赋值给b1再将b1自动装箱
+// char Charcter byete Btye short Short long Long 
+//float Float double Double
+char c = 'a';
+Character c1 = 'a';
+	}
+
+}
